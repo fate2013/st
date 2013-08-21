8612e254cdfc955acbbf963d9df7140abb89156e
zhangkh <zhangkh@zhangkh-jinshan.(none)>
2013-08-21 15:47:28 +0800

    aaaaaaaa

---
+++ protected/models/UserActivity.php
@@ -0,0 +1,25 @@
+<?php
+class UserActivity extends CActiveRecord
+{
+    public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'user_activities';
+	}
+
+        public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('id, uid, aid', 'safe', 'on'=>'search'),
+		);
+	}
+}
